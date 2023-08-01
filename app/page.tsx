774805699b9fa26ba93a5a2f81ef2a03c211a15c
Brian Walters <waltersbrian68@gmail.com>
2023-08-01 15:58:26 -0400

    working on auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+"use client";
 // import Image from "next/image";
-import TestData from "../components/TestData";
+// import TestData from "../components/TestData";
+import { useSession } from "next-auth/react";
+import AccessDenied from "../components/AccessDenied";
 
 export default function Home() {
+  const { session, loading } = useSession() as any;
+  console.log("session:", session);
+  console.log("loading:", loading);
+
+  // When rendering client side don't display anything until loading is complete
+  if (typeof window !== "undefined" && loading) return null;
+
+  // If no session exists, display access denied message
+  if (!session) {
+    return <AccessDenied />;
+  }
+
+  // If session exists, display content
   return (
     <div>
       <h1>Hello World!</h1>
-      <TestData />
+      {/* <TestData /> */}
+      <h1>Protected Page</h1>
+      <p>
+        <strong>Welcome {session.user.name}</strong>
+      </p>
     </div>
   );
 }
